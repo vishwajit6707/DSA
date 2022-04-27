89562b5fcf81101290e579bd6b93d5e18c88d842
vishwajit6707 <67246359+vishwajit6707@users.noreply.github.com>
2022-04-27 11:01:50 +0530

    Update and rename Reverse String to Reverse String.java

--- Leetcode/Reverse String.java
+++ Leetcode/Reverse String.java
@@ -15,3 +15,20 @@ class Solution {
         }
     }
 }
+
+
+//second Approach 
+
+class Solution {
+    public void reverseString(char[] s) {
+       int start = 0; 
+       int end = s.length-1;
+        while(start<end){
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
