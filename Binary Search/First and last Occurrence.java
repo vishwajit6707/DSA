908d5563dbe1d7c4d8967034afda799554357d00
vishwajit6707 <67246359+vishwajit6707@users.noreply.github.com>
2022-04-30 23:19:54 +0530

    Create First and last Occurrence.java

---
+++ Binary Search/First and last Occurrence.java
@@ -0,0 +1,64 @@
+package pp;
+
+public class binary {
+	//for first occurrence
+	public static int first(int nums[],int target) {
+		int start = 0;
+		int end = nums.length-1;
+	int result = -1;
+		while(start<=end) {
+			int mid = start + (end-start)/2;
+			if(mid == 0 || nums[mid]==target && target > nums[mid-1] ) {
+				return mid;
+				}
+			else if(nums[mid]>target || nums[mid]==target)  {
+					end = mid-1;
+				}
+			else if(nums[mid]<target) 
+			{
+				start = mid +1;
+			}
+			else {
+				end = mid-1;
+			}
+		}
+
+		return result;
+	}
+	//for last occurrence
+	public static int last(int nums[],int target) {
+		int start = 0;
+		int end = nums.length-1;
+	int result = -1;
+		while(start<=end) {
+			int mid = start + (end-start)/2;
+			if(mid == nums.length-1) return mid;
+			if( nums[mid]==target && target < nums[mid+1] ) {
+				return mid;
+				}
+			else if(nums[mid]>target )  {
+					end = mid-1;
+				}
+			else if(nums[mid]<target || nums[mid]==target) 
+			{
+				start = mid +1;
+			}
+			else {
+				end = mid-1;
+			}
+		}
+
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		int n[] = {1,1,1,3,4,4,4,4,5,6,7};
+		int k = 1;
+		int ans = first(n,k);
+		int ans2 = last(n,k);
+		System.out.println("first occurrence is " + ans);
+		System.out.println("second occurrence is " +ans2);
+
+		
+	}
+}
