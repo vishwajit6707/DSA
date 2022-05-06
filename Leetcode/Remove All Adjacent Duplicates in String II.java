6f95a40f524771033df4fe8cb7c42994c1da8d41
vishwajit6707 <67246359+vishwajit6707@users.noreply.github.com>
2022-05-06 23:22:08 +0530

    Update Remove All Adjacent Duplicates in String II.java

--- Leetcode/Remove All Adjacent Duplicates in String II.java
+++ Leetcode/Remove All Adjacent Duplicates in String II.java
@@ -20,3 +20,58 @@ class Solution {
         return s;
     }
 }
+
+//Better Approach...
+
+
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        Stack<int[]> main = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(!main.isEmpty() && main.peek()[0]==c){
+                main.peek()[1]++;
+            }
+            else{
+                main.push(new int[]{c,1});
+            }
+            if(main.peek()[1]==k){
+                main.pop();
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        while(!main.isEmpty()){
+            int[] top = main.pop();
+            
+            while(top[1]-->0){
+                result.append((char) top[0]);
+            }
+            
+        }
+        return result.reverse().toString();
+    }
+}class Solution {
+    public String removeDuplicates(String s, int k) {
+        Stack<int[]> main = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(!main.isEmpty() && main.peek()[0]==c){
+                main.peek()[1]++;
+            }
+            else{
+                main.push(new int[]{c,1});
+            }
+            if(main.peek()[1]==k){
+                main.pop();
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        while(!main.isEmpty()){
+            int[] top = main.pop();
+            
+            while(top[1]-->0){
+                result.append((char) top[0]);
+            }
+            
+        }
+        return result.reverse().toString();
+    }
+}
