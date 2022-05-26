e26900db3bcb2c83488572bcdecdd498b623e72b
vishwajit6707 <67246359+vishwajit6707@users.noreply.github.com>
2022-05-26 13:31:54 +0530

    Create Maximum Subarray.java

---
+++ Leetcode/Maximum Subarray.java
@@ -0,0 +1,43 @@
+https://leetcode.com/problems/maximum-subarray/
+
+
+//brute force apprach - with TLE Error.
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if(nums.length == 1 ) return nums[0];
+        if(nums.length == 0 ) return 0;
+     int max =Integer.MIN_VALUE;
+		   for(int i = 0; i<nums.length; i++) {
+			   for(int j = i; j<nums.length;j++) {
+				   int count= 0;
+				   for(int k = i; k<=j ;k++ ) {
+					   count = count + nums[k];
+				   }
+				   max = Math.max(max, count);
+			   }
+		   }
+		 return max;   
+    }
+}
+
+
+
+
+
+//best approach kadane's algorithm. 
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        	 int max =nums[0];
+		 int maxSofar = 0;
+		 for(int i = 0; i<nums.length ; i++) {
+			 maxSofar = maxSofar + nums[i];
+			 if(maxSofar<nums[i]) {
+				 maxSofar = nums[i];
+			 }
+            max =  Math.max(max,maxSofar);
+		 }
+		 return max;
+    }
+}
