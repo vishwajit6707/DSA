1837af44224c0e06633ef4f21a5c13706911f7e6
vishwajit6707 <67246359+vishwajit6707@users.noreply.github.com>
2022-05-26 13:38:03 +0530

    Update Maximum Subarray(kadane's algorithm).java

--- Leetcode/Maximum Subarray(kadane's algorithm).java
+++ Leetcode/Maximum Subarray(kadane's algorithm).java
@@ -8,20 +8,19 @@ class Solution {
     public int maxSubArray(int[] nums) {
         if(nums.length == 1 ) return nums[0];
         if(nums.length == 0 ) return 0;
-     int max =Integer.MIN_VALUE;
-		   for(int i = 0; i<nums.length; i++) {
-			   for(int j = i; j<nums.length;j++) {
+        int max =Integer.MIN_VALUE;
+	 	   for(int i = 0; i<nums.length; i++){
+			   for(int j = i; j<nums.length;j++){
 				   int count= 0;
-				   for(int k = i; k<=j ;k++ ) {
+				   for(int k = i; k<=j ;k++ ){
 					   count = count + nums[k];
 				   }
 				   max = Math.max(max, count);
-			   }
-		   }
-		 return max;   
+               }
+           }
+        return max;   
     }
 }
-
 
 //best approach kadane's algorithm. 
 // Time Complexity = O(N).
