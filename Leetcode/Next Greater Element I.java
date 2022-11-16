771e915aa45a790eb7685334ede4084b16302bcc
vishwajit6707 <67246359+vishwajit6707@users.noreply.github.com>
2022-11-17 01:00:55 +0530

    Update Next Greater Element I.java

--- Leetcode/Next Greater Element I.java
+++ Leetcode/Next Greater Element I.java
@@ -1,25 +1,35 @@
-//https://leetcode.com/problems/next-greater-element-i/
+// Next Greater Element I
+// Leetcode Link - https://leetcode.com/problems/next-greater-element-i/
 
-//Brut force approach....
-
+// Time Complexity - O(N).
+// Space Complexity - O(N).
 
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-     int[] kk = new int[nums1.length];
-        for(int i = 0 ; i<nums1.length; i++){
-            kk[i]= -1;
+        Stack<Integer> st = new Stack<>();
+        int arr[] = new int[nums2.length];
+        HashMap<Integer , Integer>map = new HashMap<>();
+        for(int i = nums2.length-1 ; i>= 0 ; i--){
+            while(!st.isEmpty() && st.peek()<nums2[i]){
+                st.pop();
+            }
+            if(!st.isEmpty()){
+              arr[i] = st.peek();  
+            } 
+            else{
+                arr[i] = -1;
+            }
+            st.push(nums2[i]);
         }
-        
-        
-        for(int i = 0; i<nums1.length; i++){
-         for(int j = 0 ; j<nums2.length; j++){
-           if(nums1[i] == nums2[j]){
-              for(int k = j+1 ; k<nums2.length; k++){
-                    if(nums2[k]>nums1[i]){
-                                kk[i] = nums2[k];
-                        break;
-                  }
-           }
-         } 
-       }
-     }
+        //for storing the index of the elements from nums1
+        for(int i = 0; i<nums1.length ; i++){
+            map.put(nums1[i],i);
+        }
+
+        int ans[] = new int[nums1.length];
+        for(int i = 0; i<nums2.length ; i++){
+            if(map.containsKey(nums2[i])) ans[map.get(nums2[i])] = arr[i];
+        }
+        return ans;
+    }
+}
