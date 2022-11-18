54e352ba1e1c2fbf42dda710ff8d94458dee9211
vishwajit6707 <67246359+vishwajit6707@users.noreply.github.com>
2022-11-18 12:41:56 +0530

    Create Ugly Number.java

---
+++ Leetcode/Ugly Number.java
@@ -0,0 +1,20 @@
+// Ugly Number.
+// Leetcode Link - https://leetcode.com/problems/ugly-number/
+
+// Time Complexity - O(LogN).
+// Space Complexity - O(1).
+
+class Solution {
+    public boolean isUgly(int n) {
+        if(n==0) return false;
+        if(n==1) return true;
+        else if(n % 5 == 0){
+            return isUgly(n/5);
+        }else if(n % 3 == 0){
+            return isUgly(n/3);
+        }else if(n % 2 == 0){
+            return isUgly(n/2);
+        }
+        return false;
+    }
+}
