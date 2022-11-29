ecc7b2adec642768c82de843af23e5df5cde9fb5
vishwajit6707 <67246359+vishwajit6707@users.noreply.github.com>
2022-11-29 18:47:59 +0530

    Update Fibonacci Number.java

--- Leetcode/Fibonacci Number.java
+++ Leetcode/Fibonacci Number.java
@@ -1,24 +1,16 @@
-//https://leetcode.com/problems/fibonacci-number/
-
-//Dynamic Programming (Recursion + Memoization)
+// Fibonacci Number.
+// Leetcode Link - https://leetcode.com/problems/fibonacci-number/
 
 class Solution {
- 
-    
-   public int a(int n, int k[]) {
-	if(n == 1 || n == 0) return n;
-
-	if(k[n]==-1) {
-	    return k[n] = a(n-1, k)+ a(n-2, k);
-	}
-	  return k[n];
- }
-	
-	public int fib(int n) {
-		int k[]= new int[n+1];
-                for(int i =0 ; i<k.length; i++){
-                      k[i]= -1;
-                }
-		return a(n, k);
-	 }
+    int dp[];
+    public int fib(int n) {
+        dp = new int[n+1];
+        return ans(n);
+    }
+    public int ans(int n){
+        if(dp[n] != 0) return dp[n];
+        if(n == 0) return 0;
+        if(n == 1)return 1;
+        return dp[n] = ans(n-1) + ans(n-2);
+    }
 }
