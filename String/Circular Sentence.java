50522d0ea4308c0774cae11a3ca385664b8cf063
vishwajit6707 <67246359+vishwajit6707@users.noreply.github.com>
2022-12-05 14:11:39 +0530

    Update Circular Sentence.java

--- String/Circular Sentence.java
+++ String/Circular Sentence.java
@@ -3,10 +3,8 @@
 
 class Solution {
     public boolean isCircularSentence(String sentence) {
-        for(int i = 0; i<sentence.length(); i++){
-            if(sentence.charAt(i)==' ' && i>1 && i<sentence.length()-1){
-                if(sentence.charAt(i-1) != sentence.charAt(i+1)) return false;
-            }
+        for(int i = 1; i<sentence.length()-1; i++){
+            if(sentence.charAt(i)==' ' && sentence.charAt(i-1) != sentence.charAt(i+1)) return false;
         }
         if(sentence.charAt(sentence.length()-1) != sentence.charAt(0)) return false;
         return true;
